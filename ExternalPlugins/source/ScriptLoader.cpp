1b48b4bf62ef26bd22674ac38d195327490ccb6e
deadcodes <86150272+deadcodes@users.noreply.github.com>
2025-06-25 12:00:24 +0530

    - Update to ME 1.319 - Linked Sol for Debug - Added compat for Plugin Manager(WIP)

--- ExternalPlugins/source/ScriptLoader.cpp
+++ ExternalPlugins/source/ScriptLoader.cpp
@@ -1,5 +1,11 @@
+#pragma once
+
 #include "ScriptLoader.h"
 #include <vector>
+#include <wtypes.h>
+#include <string>
+#include "imgui.h"
+#include <API.h>
 
 extern "C" __declspec(dllexport) void GetScripts(std::shared_ptr<ScriptFunction[]>*functions, size_t * count) {
     std::initializer_list<ScriptFunction> initList = {
@@ -21,4 +27,36 @@ extern "C" __declspec(dllexport) void GetScripts(std::shared_ptr<ScriptFunction[
     std::copy(initList.begin(), initList.end(), functionArray.get());
 
     *functions = functionArray;
+}
+
+extern "C" __declspec(dllexport) void LoadDLL(HWND parent) {
+    DWORD processId;
+    GetWindowThreadProcessId(parent, &processId);
+    std::string processIdStr = std::to_string(processId);
+    MessageBoxA(nullptr, processIdStr.c_str(), "LoadDLL", MB_OK | MB_ICONERROR);
+    return;
+}
+
+extern "C" __declspec(dllexport) void RenderUI() {
+    ImGuiContext* ctx = GetImGuiContext();
+    if (!ctx) {
+        return;  // Exit if we can't get a valid context
+    }
+    ImGui::SetCurrentContext(ctx);
+	ImGui::Begin("External Plugin UI");
+    ImGui::Text("This is an external plugin UI example.");
+	ImGui::Text("You can add your own UI elements here.");
+    if(ImGui::Button("Unload Plugin")) {
+        g_unloadRequested = true;
+    }
+
+    ImGui::End();
+}
+
+extern "C" __declspec(dllexport) void onUnload() {
+    MessageBoxA(nullptr, "onUnload", "onUnload", MB_OK | MB_ICONERROR);
+}
+
+extern "C" __declspec(dllexport) bool UnloadRequested() {
+    return g_unloadRequested;
 }
